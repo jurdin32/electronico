c078b537d1a109666730cda35abb109935e9dec5
johnny <urdin32@gmail.com>
2021-11-12 10:47:13 -0500

    Creacion de ordenes de trabajo

--- OrdenesTrabajo/models.py
+++ OrdenesTrabajo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from Personas.models import Clientes
+
+
+class Orden(models.Model):
+    fecha=models.DateTimeField(auto_now_add=True)
+    secuencial=models.CharField(max_length=9, default='000000000')
+    cliente=models.ForeignKey(Clientes,on_delete=models.CASCADE)
+    dispositivo_recibido=models.TextField()
+    marca_modelo=models.TextField(null=True,blank=True)
+    password=models.CharField(max_length=50,null=True,blank=True)
+    numero_serie=models.CharField(max_length=50,null=True,blank=True)
+    cables_accesorios=models.TextField(null=True,blank=True)
+    problema_reportado=models.TextField(null=True,blank=True)
+    tareas_a_realizar=models.TextField(null=True,blank=True)
+    observaciones=models.TextField(null=True,blank=True)
+    hora_fecha_salida=models.DateTimeField(null=True,blank=True)
+    estado=models.CharField(max_length=60,default="EN REVISIÓN")
--- electronico/settings.py
+++ electronico/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'Personas',
     'Productos',
     'DocumentosElectronicos',
+    'OrdenesTrabajo',
 
 ]
 
