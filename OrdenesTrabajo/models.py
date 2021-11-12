f2942af0408e1a6a88f24051accbadfb7531b942
johnny <urdin32@gmail.com>
2021-11-12 10:53:30 -0500

    Creacion de ordenes de trabajo

--- OrdenesTrabajo/models.py
+++ OrdenesTrabajo/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 # Create your models here.
+from Facturacion.models import DatosFacturacion, Empresa
 from Personas.models import Clientes
 
 
 class Orden(models.Model):
+    empresa=models.ForeignKey(Empresa,on_delete=models.CASCADE,null=True,blank=True)
     fecha=models.DateTimeField(auto_now_add=True)
     secuencial=models.CharField(max_length=9, default='000000000')
     cliente=models.ForeignKey(Clientes,on_delete=models.CASCADE)
@@ -17,4 +19,14 @@ class Orden(models.Model):
     tareas_a_realizar=models.TextField(null=True,blank=True)
     observaciones=models.TextField(null=True,blank=True)
     hora_fecha_salida=models.DateTimeField(null=True,blank=True)
+    total=models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
+    abono=models.DecimalField(max_digits=9, decimal_places=2, default=0.00)
+    total_cobrar=models.DecimalField(max_digits=9,decimal_places=2, default=0.00)
     estado=models.CharField(max_length=60,default="EN REVISIÓN")
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        datos=DatosFacturacion.objects.get(empresa=self.empresa)
+        ordenes=Orden.objects.filter(empresa=self.empresa).count()+datos.secuencial+1
+        self.secuencial=str.zfill(ordenes,9)
+        super(Orden, self).save()
