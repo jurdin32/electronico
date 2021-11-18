30f54349caea2cb5d2939a3b18eaeef7a27cde6d
Johnny <urdin32@gmail.com>
2021-11-18 16:01:00 -0500

    Creacion de ordenes de trabajo

--- OrdenesTrabajo/views.py
+++ OrdenesTrabajo/views.py
@@ -35,9 +35,11 @@ def ordenes_trabajo(request):
         orden.total=request.POST.get('total') or 0
         orden.abono=request.POST.get('abono') or 0
         orden.total_cobrar=request.POST.get('total_cobrar') or 0
-        if float(request.POST.get('total_cobrar').replace(",","."))==float('0.00'):
+        print(float(request.POST.get('total'))==float(request.POST.get('total')))
+        if float(request.POST.get('total'))==float(request.POST.get('total')):
             orden.estado="PAGADO"
-        orden.estado=request.POST.get('estado') or "POR COBRAR"
+        else:
+            orden.estado=request.POST.get('estado') or "POR COBRAR"
         orden.save()
         print(orden)
 
