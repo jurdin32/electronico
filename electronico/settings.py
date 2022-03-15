3b6dd27d535d75cd243d1a78b311e5146a41d2f4
Johnny Urdin <urdin32@gmail.com>
2022-03-15 11:07:48 -0500

    Cambios en la vista de cuentas por cobrar

--- electronico/settings.py
+++ electronico/settings.py
@@ -92,14 +92,14 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-#STATIC_ROOT='static'
+STATIC_ROOT='/mnt/07EBF45679F193CD/var/www/electronico/static'
 
-STATICFILES_DIRS=[
-   'static',
-]
+# STATICFILES_DIRS=[
+#    'static',
+# ]
 
 MEDIA_URL='/media/'
-MEDIA_ROOT="media"
+MEDIA_ROOT="/mnt/07EBF45679F193CD/var/www/electronico/media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
