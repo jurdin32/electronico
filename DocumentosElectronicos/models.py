813858679468d234caa99fe71d7dacae31d69603
Johnny Urdin <urdin32@gmail.com>
2022-03-24 09:29:37 -0500

    Cambio en la visualizacion de facturas

--- DocumentosElectronicos/models.py
+++ DocumentosElectronicos/models.py
@@ -4,7 +4,7 @@ from Personas.models import Clientes
 from Productos.models import Producto
 
 class Factura(models.Model):
-    ambiente=models.CharField(max_length=40, default="PRUEBAS")
+    ambiente=models.CharField(max_length=40, default="NO APLICA")
     tipo=models.CharField(max_length=20, default="FACTURA")
     formaPago=models.ForeignKey(FormasPagos,on_delete=models.CASCADE, default=1)
     empresa=models.ForeignKey(Empresa,on_delete=models.CASCADE,null=True,blank=True)
